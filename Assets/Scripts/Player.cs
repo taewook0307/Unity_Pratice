2eb8b7988049e5c0ece2b3d1b688acd35e5691d9
Kim Tae Wook <gmill0411@naver.com>
2025-01-06 15:46:52 +0900

    이동

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,17 +7,32 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    private void FixedUpdate()
-    {
-        
+        playerRigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+
+    #region Move
+
+    [SerializeField]
+    private float moveSpeed;
+
+    Rigidbody playerRigid;
+
+    private void Move()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        float dirY = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveVector = new Vector3(dirX, 0, dirY).normalized;
+        Vector3 newVector = transform.position + moveVector * moveSpeed * Time.deltaTime;
+        playerRigid.Move(newVector, transform.rotation);
+    }
+
+    #endregion
 }
