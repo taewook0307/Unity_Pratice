b941a1ddaec4a0de1775074b2d19f13ade048602
Kim Tae Wook <gmill0411@naver.com>
2025-01-08 12:22:21 +0900

    이동 및 회전

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,28 +10,75 @@ public class Player : MonoBehaviour
         playerRigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         Move();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRot();
+    }
 
     #region Move
 
+    private Rigidbody playerRigid;
+    
     [SerializeField]
     private float moveSpeed;
-
-    Rigidbody playerRigid;
 
     private void Move()
     {
         float dirX = Input.GetAxisRaw("Horizontal");
         float dirY = Input.GetAxisRaw("Vertical");
 
-        Vector3 moveVector = new Vector3(dirX, 0, dirY).normalized;
-        Vector3 newVector = transform.position + moveVector * moveSpeed * Time.deltaTime;
-        playerRigid.Move(newVector, transform.rotation);
+        Vector3 moveHorizontalVector = transform.right * dirX;
+        Vector3 moveVerticalVector = transform.forward * dirY;
+        Vector3 dirVector = (moveHorizontalVector + moveVerticalVector).normalized;
+        Vector3 movePosition = transform.position + dirVector * moveSpeed * Time.deltaTime;
+
+        playerRigid.MovePosition(movePosition);
+    }
+
+    #endregion
+
+    #region Camera
+
+    [SerializeField]
+    private Camera mainCam;
+
+    [SerializeField]
+    private float horizontalSensitivity;
+
+    [SerializeField]
+    private float verticalSensitivity;
+
+    private float limitAngle = 90f;
+    private float curVerticalAngle = 0;
+
+    private void CameraRot()
+    {
+        HorizontalRot();
+        VerticalRot();
+    }
+
+    private void HorizontalRot()
+    {
+        float angle = Input.GetAxis("Mouse X");
+        Vector3 yRotation = new Vector3(0, angle, 0) * horizontalSensitivity;
+
+        playerRigid.MoveRotation(transform.rotation * Quaternion.Euler(yRotation));
+    }
+
+    private void VerticalRot()
+    {
+        float angle = Input.GetAxisRaw("Mouse Y");
+        float xRotation = angle * verticalSensitivity;
+        curVerticalAngle -= xRotation;
+        curVerticalAngle = Mathf.Clamp(curVerticalAngle, -limitAngle, limitAngle);
+
+        mainCam.transform.localEulerAngles = new Vector3(curVerticalAngle, 0, 0);
     }
 
     #endregion
